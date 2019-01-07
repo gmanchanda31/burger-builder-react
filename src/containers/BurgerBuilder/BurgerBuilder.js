6fa5dc15e09f61bee676eee7e410ec623b3becc0
Gourav Manchanda <gouravmanchanda28@gmail.com>
2019-01-07 05:52:51 +0530

    checkout functionality added

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -74,7 +74,18 @@ class BurgerBuilder extends Component {
     //     console.log(error);
     //     this.setState({ loading: false, purchasing: false });
     //   });
-    this.props.history.push('/checkout')
+    const queryParams = [];
+
+    for (let i in this.state.ingredients) {
+      queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]))
+    }
+
+    const queryString = queryParams.join('&');
+
+    this.props.history.push({ 
+      pathname: "/checkout",
+      search: '?' + queryString
+    });
   }
 
   updatePurchaseState (ingredients) {
